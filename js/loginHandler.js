e0c00234b535c1f99751b71e42893797d6c12928
Kraken1911 <142685016+Kraken1911@users.noreply.github.com>
2024-11-19 13:10:25 -0600

    cambios

---
+++ js/loginHandler.js
@@ -0,0 +1,9 @@
+var loginemail = document.getElementById("emailinputlogin");
+var loginpasswd = document.getElementById("emailinputlogin");
+var rememberme = document.getElementById("rememberme");
+var loginsubmitbtn= document.getElementById("loginsubmit");
+var user={
+    
+}
+
+
