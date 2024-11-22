10f592b90b9b6870db2d157d2e0d9295471facc1
Kraken1911 <142685016+Kraken1911@users.noreply.github.com>
2024-11-22 14:24:21 -0600

    se intentara crear el fokin page de doctor y paciente

--- js/loginHandler.js
+++ js/loginHandler.js
@@ -1,7 +1,9 @@
 const pacientUser="juanito1213@gmail.com";
 const pacientpasswd = "JsonMandela";
+const pacientName="Julio burgos"
 const doctorUser="damiancervantes@gmail.com"
-const doctorpasswd="llaveDeAcceso"
+const doctorpasswd="llaveDeAcceso";
+const doctorname="Mikel Cervantes";
 var loginemailinput = document.getElementById("");
 var passwdinput = document.getElementById("");
 
